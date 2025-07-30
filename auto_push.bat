95893122e11cf5ee5283876acaf35bf47b77fcb0
o25042124-boop <o25042124@gmail.com>
2025-07-30 21:39:54 +0900

    Fix: Ensure index.html present in all folders

--- auto_push.bat
+++ auto_push.bat
@@ -3,6 +3,6 @@ cd /d "C:\Users\User\Documents\nibannkeiei-podcast-ranking-viewer"
 python rank_log.py
 
 git pull --rebase origin main
-git add data.json
-git commit -m "自動更新 %date% %time%"
+git add .
+git commit -m "Daily auto-update"
 git push origin main
