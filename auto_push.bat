7821aa599c302c15c9d63b032f7b4de83d41a4d7
o25042124-boop <o25042124@gmail.com>
2025-07-30 22:14:56 +0900

    Daily auto-update

--- auto_push.bat
+++ auto_push.bat
@@ -2,6 +2,7 @@
 cd /d "C:\Users\User\Documents\nibannkeiei-podcast-ranking-viewer"
 python rank_log.py
 
-git add .
-git commit -m "Daily auto-update"
-git push
+git pull --rebase origin main
+git add data.json
+git commit -m "自動更新 %date% %time%"
+git push origin main
